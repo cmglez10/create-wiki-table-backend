ff07312fe5b902c583c9c5a2a3019a743e3ccfc9
César González <cesargonzaleziniguez@teamitg.com>
2023-01-28 10:53:28 +0100

    configure cors

--- index.ts
+++ index.ts
@@ -2,9 +2,11 @@ import Koa from "koa";
 import Router from "koa-router";
 import Cheerio from "cheerio";
 import axios from "axios";
+import cors from "@koa/cors";
 
 const app: Koa = new Koa();
 const router = new Router();
+app.use(cors())
 
 router.get("/analyze/:url", async (ctx: Koa.Context) => {
   const url: string = ctx.params.url;
